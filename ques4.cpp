b7afb0dbcd17ca01adf0ece96f09164eb1d7ebf8
davishrahul <36623942+davishrahul@users.noreply.github.com>
2019-07-09 22:03:52 +0530

    Create ques4.cpp

---
+++ ques4.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int number;
+    cin>>number;
+    int a[number];
+    for(int i=0;i<number;i++)
+    {
+        cin>>a[i];
+    }
+    int temp;
+    for(int i=0;i<number;i++)
+    {
+       for(int j=i+1;j<number;j++)
+       {
+           if(a[i]>a[j])
+           {
+               temp=a[i];
+               a[i]=a[j];
+               a[j]=temp;
+           }
+       }     
+    }
+    for(int i=0;i<number;i++)
+    {
+        cout<<a[i]<<" ";
+    }
+
+}
