be9525742b5ecd53fee50c13ca50c238e172218f
davishrahul <36623942+davishrahul@users.noreply.github.com>
2019-07-09 23:11:43 +0530

    Create ques7.cpp

---
+++ ques7.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    int count=0;
+    string s;
+    cin>>s;
+    for(int i=0;i<s.length();i++)
+    {
+        if((s[i]>='0' && s[i]<='9') || s[i]=='.')
+        {
+          count++;
+        }
+    }
+    if(count>=1)
+    {
+        cout<<"yes";
+    }
+    else
+    {
+        cout<<"no";
+    }
+    
+}
