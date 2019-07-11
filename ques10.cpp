ad809f22b1b1fc706fdfc2b6339f7be8c1f6ec59
davishrahul <36623942+davishrahul@users.noreply.github.com>
2019-07-11 15:10:44 +0530

    Update ques10.cpp

--- ques10.cpp
+++ ques10.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main()
 {
     int sub_hour,sub_minutes,hour1,hour2,minutes1,minutes2;
-    cin>>hour1>>hour2>>minutes1>>minutes2;
+    cin>>hour1>>minutes1>>hour2>>minutes2;
     sub_hour=hour1-hour2;
     sub_minutes=minutes1-minutes2;
     if(sub_hour<0||sub_minutes<0)
