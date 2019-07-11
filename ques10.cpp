2d6b71845c47a1169ec8bb09abbb1ed7aeb500ed
davishrahul <36623942+davishrahul@users.noreply.github.com>
2019-07-11 14:53:46 +0530

    Create ques10.cpp

---
+++ ques10.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int sub_hour,sub_minutes,hour1,hour2,minutes1,minutes2;
+    cin>>hour1>>hour2>>minutes1>>minutes2;
+    sub_hour=hour1-hour2;
+    sub_minutes=minutes1-minutes2;
+    if(sub_hour<0||sub_minutes<0)
+    {
+        sub_hour=-sub_hour;
+        sub_minutes=-sub_minutes;    
+        cout<<sub_hour<<" "<<sub_minutes;
+    }
+    else
+    {
+        cout<<sub_hour<<" "<<sub_minutes;
+    }   
+    return 0;
+}
